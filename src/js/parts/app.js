8f9868f7e67c70a95041745e637fec237225968e
ravisondagar <ravis@karmadhi.com>
2023-08-16 17:35:11 +0530

    josephs

---
+++ src/js/parts/app.js
@@ -0,0 +1,19 @@
+import "slick-carousel";
+
+export class App {
+  init() {
+    // $('.main-slider').slick({
+    //   infinite: true,
+    //   slidesToShow: 1,
+    //   dots: true,
+    //   fade: true,
+    //   arrows: true,
+    //   slidesToScroll: 1,
+    //   autoplay: true,
+    //   autoplaySpeed: 2000,
+    //   prevArrow: $(".hero-home .slick--prev"),
+    //   nextArrow: $(".hero-home .slick--next"),
+    // });
+  }
+  slickSLider() { }
+}
